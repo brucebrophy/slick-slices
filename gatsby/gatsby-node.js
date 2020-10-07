105a87ad678bca39482a89f871e9c9cbedcace52
Bruce Brophy <bruce@brucebrophy.com>
2020-10-06 22:04:21 -0400

    Set up for pagination

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -31,6 +31,41 @@ async function turnPizzasIntoPages({ graphql, actions }) {
   });
 }
 
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+  // query all pizzas
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          id
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  const pageSize = parseInt(process.env.GATSBY_PER_PAGE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+
+  Array.from({ length: pageCount }).forEach((_, index) => {
+    actions.createPage({
+      path: `/slicemasters/${index + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      context: {
+        skip: index * pageSize,
+        currentPage: index + 1,
+        pageSize,
+      },
+    });
+  });
+
+  console.log(`There are ${pageCount} pages with ${pageSize} per page`);
+}
+
 async function fetchBeersAndTurnIntoNodes({
   actions,
   createNodeId,
@@ -94,5 +129,6 @@ export async function createPages(params) {
   await Promise.all([
     turnPizzasIntoPages(params),
     turnToppingsIntoPages(params),
+    turnSlicemastersIntoPages(params),
   ]);
 }
--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,83 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-const SliceMastersPage = () => (
-  <>
-    <h1>Slice Masters</h1>
-  </>
-);
+const SliceMasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SliceMasterCard = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    transform: rotate(1deg);
+    background: var(--yellow);
+    padding: 2rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    text-align: center;
+  }
+`;
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        id
+        name
+        slug {
+          current
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        description
+      }
+    }
+  }
+`;
+
+const SliceMastersPage = ({ data }) => {
+  const slicemasters = data.slicemasters.nodes;
+  return (
+    <>
+      <SliceMasterGrid>
+        {slicemasters.map((person) => (
+          <SliceMasterCard key={person.id}>
+            <Link to={`slicemasters/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </SliceMasterCard>
+        ))}
+      </SliceMasterGrid>
+    </>
+  );
+};
 
 export default SliceMastersPage;
